8c6e8c38e34cb3be16a9c86c7bd4899c2f1e1e5f
Mohammad Davatgar <md2002.110@gmail.com>
2024-12-03 14:21:51 +0330

    left over from last commit

--- pkg/models/message.go
+++ pkg/models/message.go
@@ -10,3 +10,11 @@ type PvMessage struct {
 	SeenAt    time.Time `db:"seen_at"`
 	CreatedAt time.Time `db:"created_at"`
 }
+
+type GroupMessage struct {
+	Id        uint64 `db:"id"`
+	GroupId   uint64 `db:"group_id"`
+	Message   string `db:"message"`
+	SenderId  uint64 `db:"sender_id"`
+	CreatedAt uint64 `db:"created_at"`
+}
